e5eec140bed83261acfcc0951a69089fdca02c89
Alexander Guryanov <caiiiycuk@gmail.com>
2021-10-15 10:41:59 +0700

    Remove useless files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import GameStudio from './game-studio';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GameStudio />
   </React.StrictMode>,
   document.getElementById('root')
 );
